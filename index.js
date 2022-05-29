069772b603d0209219c0b032d9ed45392cbd2730
sazzad <sazzadkarim01@gmail.com>
2022-05-29 12:49:08 +0600

    function setup done

--- index.js
+++ index.js
@@ -6,13 +6,25 @@ const port = process.env.PORT || 5000;
 const app = express();
 const jwt = require("jsonwebtoken");
 
-
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5eiql.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
+async function run() {
+  try {
+    await client.connect();
+    const productsCollection = client.db("partsCollection").collection("parts");
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Computer Parts SERVER Side!");
