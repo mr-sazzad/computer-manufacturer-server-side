f95eba1e040e39c9830e5b63f03c2afce2ce7747
sazzad <sazzadkarim01@gmail.com>
2022-05-29 12:44:30 +0600

    first commit with setup

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+const jwt = require("jsonwebtoken");
+
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5eiql.mongodb.net/?retryWrites=true&w=majority`;
+
+
+
+app.get("/", (req, res) => {
+  res.send("Computer Parts SERVER Side!");
+});
+
+app.listen(port, () => {
+  console.log(`Parts manufacture listening on port ${port}`);
+});
