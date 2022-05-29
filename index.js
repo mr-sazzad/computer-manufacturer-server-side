4a85f9ae9f472fcc440ca0bd6a51a6aded9b18da
sazzad <sazzadkarim01@gmail.com>
2022-05-29 13:06:42 +0600

    api created for get all users

--- index.js
+++ index.js
@@ -72,6 +72,12 @@ async function run() {
     });
 
 
+    app.get("/user", async (req, res) => {
+      const users = await usersCollection.find().toArray();
+      res.send(users);
+    });
+
+
     app.get("/admin/:email", async (req, res) => {
       const email = req.params.email;
       const user = await usersCollection.findOne({ email: email });
