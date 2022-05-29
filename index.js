2be90f60c08cfaf3c73629dd0e0cb29c43749776
sazzad <sazzadkarim01@gmail.com>
2022-05-29 12:54:53 +0600

    api create for seen who in admin

--- index.js
+++ index.js
@@ -39,6 +39,15 @@ async function run() {
       const result = await productsCollection.findOne(query);
       res.send(result);
     });
+
+
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+    
   } finally {
   }
 }
